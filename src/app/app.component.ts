606b49127b350f88e7ee4f484492f60af64d1fbe
Saklain Ahmad <saklainhmd@gmail.com>
2024-01-14 15:42:45 +0530

    uploading angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';   //we added this
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, HomeComponent,RouterModule],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/img/home.png"
+          alt="logo"
+          aria-hidden="true"
+          width="80px"
+          height="63px"
+        />
+      </header>
+      <section class="content">
+        <router-outlet></router-outlet>
+       
+      </section>
+    </main>
+  `,
+  // templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'myhomeapp';
+  title = 'homes';
 }
